027d20a4465dcdffd051b06766ca893c72d5351f
Ruddy Monlouis <r.monlouis@gmail.com>
2022-03-05 01:15:36 +0100

    add test

--- test/binance_service_test.py
+++ test/binance_service_test.py
@@ -1,3 +1,10 @@
+import unittest
 
-class BinanceServiceTest:
-    pass
+from app.service.binance_service import BinanceService
+
+
+class BinanceServiceTest(unittest.TestCase):
+
+    def test_binance_service_is_instance_of_binance_service(self):
+        binance_service = BinanceService()
+        self.assertIsInstance(binance_service, BinanceService)
